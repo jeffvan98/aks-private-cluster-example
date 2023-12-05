613559bb6de9313ffa2f00fa7489e7c8cd46e8fe
Jeff Van Nortwick <jeffvan@microsoft.com>
2023-12-05 11:27:56 -0500

    updated Cache.sh to handle private DNS

--- Cache-Bicep/Cache.sh
+++ Cache-Bicep/Cache.sh
@@ -10,12 +10,16 @@ sku=Standard
 size=C0
 endpoint=REDIS_ENDPOINT_NAME
 
+# Create Redis
+
 az redis create \
 --location $location \
 --name $name \
 --resource-group $group \
 --sku $sku \
 --vm-size $size
+
+# Create private endpoint
 
 az network private-endpoint create \
 --name $endpoint \
@@ -26,7 +30,35 @@ az network private-endpoint create \
 --group-ids redisCache \
 --connection-name $endpoint
 
+# Disable public access to Redis
+
 az redis update \
 --name $name \
 --resource-group $group \
 --set publicNetworkAccess=Disabled
+
+# Create private DNS Zone
+
+az network private-dns zone create \
+--name privatelink.redis.cache.windows.net \
+--group $group
+
+# Obtain Redis Private Endpoint IP Address
+
+interface=$(az network private-endpoint show \
+--name $endpoint --resource-group $group \
+--query "networkInterfaces[0].id" \
+-o tsv)
+
+ipaddress=$(az network nic show \
+--ids $interface \
+--query "ipConfigurations[0].privateIPAddress" \
+-o tsv)
+
+# Add Redis to Private DNS Zone
+
+az network private-dns record-set a add-record \
+--name privatelink.redis.cache.windows.net \
+--resource-group $group \
+--record-set-name $name \
+--ipv4-address $ipaddress
